8451bcdc553e69bacdc9ca85fc217724f0f0f20d
Andrew Aitken <andrew@thescorpion.co.uk>
2016-03-31 12:10:08 +0100

    Adding settings for base app and static files

--- ttsite/settings.py
+++ ttsite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+	'base',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -119,3 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    '/static/',
+]
