ab60646c0ca13ecca94e545014c073fb79075227
Andrew Aitken <andrew@thescorpion.co.uk>
2016-04-04 17:06:52 +0100

    Adding Django Front to the project with limited login

--- base/urls.py
+++ base/urls.py
@@ -1,8 +1,11 @@
-from django.conf.urls import url
+from django.conf.urls import include, url
 
 from . import views
 
 urlpatterns = [
     url(r'^products/', views.products, name='index'),
     url(r'^$', views.index, name='index'),
+	url(r'^front-edit/', include('front.urls')),
+	url(r'^login/$', 'django.contrib.auth.views.login'),
+    url(r'^logout/$', 'django.contrib.auth.views.logout'),
 ]
--- tiphereth/urls.py
+++ tiphereth/urls.py
@@ -19,4 +19,5 @@ from django.contrib import admin
 urlpatterns = [
 	url(r'', include('base.urls')),
     url(r'^admin/', admin.site.urls),
+	url(r'^front-edit/', include('front.urls')),
 ]
